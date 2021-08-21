d4228a053bcfdd73fa6fcf4d7f198d8e129ed22b
Quaris <quaris314@gmail.com>
2021-08-20 20:06:18 -0500

    Added Rectangle

---
+++ phi/other/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phi.other
+{
+   public struct Rectangle
+   {
+      public int X;
+      public int Y;
+      public int Width;
+      public int Height;
+
+      public Rectangle(int x, int y, int width, int height)
+      {
+         X = x;
+         Y = y;
+         Width = width;
+         Height = height;
+      }
+
+      public bool Contains(int x, int y)
+      {
+         return 0 <= x - X && x - X <= Width && 0 <= y - Y && y - Y <= Height;
+      }
+   }
+}
